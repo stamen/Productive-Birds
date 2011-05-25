87547eec8f46740c95b2e7a82aca194921add8dc
Michal Migurski <migurski@studio.stamen.com>
2011-05-24 22:17:19 -0700

    Pulling basic timing information for each client

---
+++ client.php
@@ -0,0 +1,73 @@
+<?php
+
+    header('content-type: text/plain');
+
+    $dbh = mysql_connect('localhost', 'time', '');
+    mysql_select_db('timetracking', $dbh);
+    
+    function client_synonyms(&$dbh, $name)
+    {
+        $q = sprintf("SELECT ca.client1 AS ca1, ca.client2 AS ca2,
+                             cb.client1 AS cb1, cb.client2 AS cb2, 
+                             cc.client1 AS cc1, cc.client2 AS cc2
+                      
+                      FROM same_clients AS ca
+                      
+                      LEFT JOIN same_clients AS cb
+                        ON cb.client1 = ca.client1 OR cb.client1 = ca.client2
+                        OR cb.client2 = ca.client1 OR cb.client2 = ca.client2
+                      
+                      LEFT JOIN same_clients AS cc
+                        ON cc.client1 = cb.client1 OR cc.client1 = cb.client2
+                        OR cc.client2 = cb.client1 OR cc.client2 = cb.client2
+                      
+                      WHERE ca.client1 = '%s' 
+                         OR ca.client2 = '%s'",
+                      
+                      mysql_real_escape_string($name, $dbh),
+                      mysql_real_escape_string($name, $dbh));
+        
+        $res = mysql_query($q, $dbh);
+        $names = array(strtolower($name));
+        
+        while($row = mysql_fetch_array($res, MYSQL_NUM))
+            foreach($row as $other)
+                if(!is_null($other))
+                    $names[] = strtolower($other);
+        
+        $names = array_unique($names);
+        return array_values($names);
+    }
+    
+    function client_days(&$dbh, $name)
+    {
+        $names = client_synonyms($dbh, $name);
+        
+        foreach($names as $i => $name)
+            $names[$i] = sprintf("'%s'", mysql_real_escape_string($name, $dbh));
+        
+        $names = join(', ', $names);
+        
+        $q = sprintf("SELECT week, SUM(days) AS days
+                      FROM utilization
+                      WHERE client IN (%s)
+                      GROUP BY week
+                      ORDER BY week",
+                      $names);
+
+        $res = mysql_query($q, $dbh);
+        $rows = array();
+        
+        while($row = mysql_fetch_array($res, MYSQL_NUM))
+            $rows[] = $row;
+        
+        return $rows;
+    }
+    
+    foreach(client_days($dbh, $_GET['name']) as $week)
+    {
+        $week['time'] = strtotime($week[0]);
+        print_r($week);
+    }
+
+?>
